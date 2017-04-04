2bc2fffd1f63e26b539a508af5eaa9d69e0f680c
coolfish <461836324@qq.com>
2017-04-04 16:48:28 +0800

    怎家链接

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 var express = require('express'),
     app = express(),
-    path = require('path');
+    path = require('path'),
+    SocketIo = require('socket.io');
 
 var port = 9019;
 
-app.listen(port, function () {
-    console.log('项目已启动，端口号为' + port);
+// 静态文件存放位置
+app.use(express.static(path.join(__dirname, 'client'), {maxAge: 86400000}));
+
+// 路由
+app.get('/ping', function (req, res) {
+    res.end('OK');
 });
+// 首页
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+// 管理
+app.get('/admin', function (req, res) {
+    res.sendFile(__dirname + '/client/admin.html');
+});
+
+//往服务器端添加socket
+var io = SocketIo.listen(app.listen(port, function () {
+    console.log('项目已启动，端口号为' + port);
+}));
+
+// 监听
+io.on('connection', function (socket) {
+    console.log('有新的链接进来了');
+    // 获取房间id
+    var roomId = 1;
+    console.log(socket.request._query);
+});
+
---
+++ client/js/index.js
@@ -0,0 +1,44 @@
+window.onload = function () {
+    // 加载socket
+    var socket = io.connect();
+    // 控制器设置
+    var controller = {
+        up: 0,
+        down: 0,
+        left: 0,
+        right: 0,
+        jump: 0,
+        hit: 0,
+        prop: 0
+    };
+    // 加入游戏
+    function joinGame (options) {
+        var userName = $('#username').val();
+        console.log('==========');
+        socket.emit('join', {
+            userName: userName,
+            controller: options
+        });
+    }
+    // 初始化游客的名字
+    function initUserName () {
+        var ts = String((new Date()).getTime()).substr(9, 4);
+        var tempUserName = '游客' + ts;
+        $('#username').val(tempUserName);
+    }
+    // 初始化 加入房间
+    function initJoin () {
+        $('#joinBtn').click(function () {
+            console.log('-=-=-=-');
+            joinGame(controller);
+        });
+    }
+    /* 初始化 */
+    function init () {
+        // 初始化游客名字
+        initUserName();
+        // 初始化链接
+        initJoin();
+    }
+    init();
+};
